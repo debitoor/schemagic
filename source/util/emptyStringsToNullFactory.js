750b625f196fc85b7b7bb67738be2b96b31a6386
Andrii Shumada <eagleeyes91@gmail.com>
2013-10-09 12:52:03 +0300

    SB-6354 re-implemented toNullEmptyFields with traverse

--- source/util/emptyStringsToNullFactory.js
+++ source/util/emptyStringsToNullFactory.js
@@ -1,43 +1,19 @@
 "use strict";
-var _ = require('underscore');
+var traverse = require('traverse');
 
 module.exports = (function() {
 
 	function toNullEmptyFields(documents) {
-		if (_.isArray(documents)) {
-			documents.map(function(document){
-				toNullEmptyFields(document);
-			});
-			toNullEmptyFieldsArrayElements(documents);
-		} else if (_.isObject(documents)) {
-			toNullEmptyFieldsDocument(documents);
-		} else if (isEmptyString(documents)) {
-			documents = null;
+		if (documents === '') {
+			return null;
 		}
-
-		return documents;
-	}
-
-	function toNullEmptyFieldsArrayElements(array) {
-		for (var i=array.length-1; i>=0; i--) {
-			if (isEmptyString(array[i])) {
-				array[i] = null;
-			}
-		}
-	}
-
-	function toNullEmptyFieldsDocument(document){
-		Object.keys(document).forEach(function(prop) {
-			if (isEmptyString(document[prop])) {
-				document[prop] = null;
-			} else {
-				toNullEmptyFields(document[prop]);
+		traverse(documents).forEach(function(value) {
+			if (value === '') {
+				this.update(null);
 			}
 		});
-	}
 
-	function isEmptyString(value) {
-		return value === '';
+		return documents;
 	}
 
 	return {
