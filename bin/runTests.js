06cb84338a9bedcb6232ebbcf75f958984a9382e
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2013-11-21 15:41:57 +0100

    Fixing teamcity build. Updated jshint.

--- bin/runTests.js
+++ bin/runTests.js
@@ -3,38 +3,20 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 process.env.APP_ENV = process.env.APP_ENV || 'development';
 
-var fs = require("fs");
-var path = require("path");
-
-runJsHint(["source", "test"], function (err) {
-	if (err) {
-		console.error("Exiting because of jsHint errors");
-		return process.exit(1);
-	}
-	console.log("No JSHint errors detected");
-	return runMocha();
-});
-
-function runMocha() {
-	var realProcessExit = process.exit;
-	process.exit = function(code){
-		setTimeout(realProcessExit.bind(process, code), 200);
-	};
-	require('./../node_modules/mocha/bin/_mocha');
+var jsHint = require("./../node_modules/jshint/src/cli.js");
+var options = { args: ['.']};
+if (process.argv.join('').indexOf('teamcity') !== -1) {
+        options.reporter = require('jshint-teamcity-reporter').reporter;
 }
-
-function runJsHint(pathsArray, callback) {
-	var jsHint = require("./../node_modules/jshint/lib/hint.js");
-	var config = JSON.parse(fs.readFileSync(path.resolve(".jshintrc"), "utf-8"));
-	var reporter = require("./../node_modules/jshint/lib/reporters/default.js").reporter;
-	var ignores = fs.readFileSync(path.resolve(".jshintignore"), "utf8").split("\n")
-		.filter(function (line) {
-			return !!line.trim(); //remove empty lines
-		})
-		.map(path.resolve);
-	var results = jsHint.hint(pathsArray, config, reporter, ignores);
-	if (results.length > 0) {
-		return callback(new Error("JSHintErrors"));
-	}
-	return callback(null);
+if (!jsHint.run(options)) {
+        return setTimeout(function () {
+                console.error("Exiting because of jsHint errors");
+                process.exit(1);
+        }, 1000);
 }
+//run Mocha
+var realProcessExit = process.exit;
+process.exit = function (code) {
+        setTimeout(realProcessExit.bind(process, code), 2000);
+};
+require('./../node_modules/mocha/bin/_mocha');
