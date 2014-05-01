244a0c399607f77e574b179ef9f6089624049a49
Allan Ebdrup <aeb@e-conomic.com>
2014-05-01 14:03:03 +0200

    SB-XXXX make schemagic public

--- test/common.js
+++ test/common.js
@@ -1,6 +1,6 @@
 global.sinon = require('sinon');
 var chai = require('chai');
-chai.config.includeStack
+chai.config.includeStack;
 global.chai = chai;
 global.expect = chai.expect;
 chai.use(require('sinon-chai'));
