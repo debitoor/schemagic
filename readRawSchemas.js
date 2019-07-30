0a64db7288120a9534de194dec693e9b34780108
maximradko <mra@debitoor.com>
2019-07-30 12:48:41 +0300

    add exlude all files starts with dot

--- readRawSchemas.js
+++ readRawSchemas.js
@@ -7,23 +7,27 @@ function readRawSchemas(schemasDirectory) {
 	var schemaFileNames = fs.readdirSync(schemasDirectory);
 	var schemas = {};
 	schemaFileNames.forEach(function (schemaFileName) {
-		var schemaName = schemaFileName.replace('.js', '');
-		var schemaFilePath = path.join(schemasDirectory, schemaFileName);
-		if (!fs.statSync(schemaFilePath).isFile()) {
-			return; //only require files (in schema dir root)
-		}
-		var schema = require(schemaFilePath);
-		var keys = Object.keys(schema);
-		if (keys.length === 1 && keys[0] === 'default') {
-			schema = schema.default;
-		}
-		schemas[schemaName] = schema;
-		if (schemaName !== 'foreignKeys') {
-			var copy = JSON.parse(JSON.stringify(schema));
-			if (!deepEqual(schema, copy)) {
-				throw new Error(`The schema ${schemaFilePath} is not JSON stringify-able`);
-			}
-		}
+				if (/^\.\w+/.test(schemaFileName)) {
+					return;
+				}
+
+				var schemaName = schemaFileName.replace('.js', '');
+				var schemaFilePath = path.join(schemasDirectory, schemaFileName);
+				if (!fs.statSync(schemaFilePath).isFile()) {
+					return; //only require files (in schema dir root)
+				}
+				var schema = require(schemaFilePath);
+				var keys = Object.keys(schema);
+				if (keys.length === 1 && keys[0] === 'default') {
+					schema = schema.default;
+				}
+				schemas[schemaName] = schema;
+				if (schemaName !== 'foreignKeys') {
+					var copy = JSON.parse(JSON.stringify(schema));
+					if (!deepEqual(schema, copy)) {
+						throw new Error(`The schema ${schemaFilePath} is not JSON stringify-able`);
+					}
+				}
 	});
 	return schemas;
 }
